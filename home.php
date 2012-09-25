3f019a3f42103448f62bff4ce7e1fbedbc49459c
Brian Fryer <brianfryer@gmail.com>
2012-09-25 17:05:45 -0500

    ...

--- home.php
+++ home.php
@@ -1,41 +1,43 @@
 <?php get_header(); ?>
-<?php get_template_part( 'promo' ); ?>
 
-    <section class="focus home loop">
+    <div class="main-column">
+        <?php get_template_part( 'promo' ); ?>
+        <section class="focus home loop">
 
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <article class="post snippet">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <article class="post snippet">
 
-            <header>
-                <h1 class="article-title">
-                    <a title="Click to view the full post" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                </h1>
-                <?php get_template_part( 'post', 'meta' ); ?>
-            </header>
+                <header>
+                    <h1 class="article-title">
+                        <a title="Click to view the full post" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h1>
+                    <?php get_template_part( 'post', 'meta' ); ?>
+                </header>
+                <hr />
+                <?php the_content(); ?>
 
-            <?php the_content(); ?>
+                <span class="full-post">
+                    <a class="btn" title="<?php the_title(); ?>" href="<?php the_permalink();?>">View full post &raquo;</a
+                </span>
 
-            <span class="full-post">
-                <a class="btn" title="<?php the_title(); ?>" href="<?php the_permalink();?>">View full post &raquo;</a
-            </span>
+            </article><!--/.post.snippet-->
+        <?php endwhile; else: ?>
+            <article class="post">
 
-        </article><!--/.post.snippet-->
-    <?php endwhile; else: ?>
-        <article class="post">
+                <p><?php _e('Sorry, there are no posts to display here.'); ?></p>
 
-            <p><?php _e('Sorry, there are no posts to display here.'); ?></p>
+            </article><!--/.post-->
+        <?php endif; ?>
 
-        </article><!--/.post-->
-    <?php endif; ?>
+            <nav class="back-and-forth">
+                <ul>
+                    <li><?php next_posts_link('&laquo; Previous Posts') ?></li>
+                    <li><?php previous_posts_link('Newer Posts &raquo;') ?></li>
+                </ul>
+            </nav><!--/.back-and-forth-->
 
-        <nav class="back-and-forth">
-            <ul>
-                <li><?php next_posts_link('&laquo; Previous Posts') ?></li>
-                <li><?php previous_posts_link('Newer Posts &raquo;') ?></li>
-            </ul>
-        </nav><!--/.back-and-forth-->
-
-    </section><!--/.focus.home.loop-->
+        </section><!--/.focus.home.loop-->
+    </div><!--/.main-column-->
 
     <section class="shoulder">
         <?php get_sidebar(); ?>
--- post-meta.php
+++ post-meta.php
@@ -1,12 +1,12 @@
 <ul class="meta">
     <li>
-        <a title="<?php the_permalink(); ?>" href="<?php the_permalink(); ?>">Permalink</a>
-    </li>
-    <li>
         <span title="This post was was published on <?php the_date( 'D, M j, Y' ); ?>"><?php echo get_the_date( 'D, M j, Y' ); ?></span>
     </li>
     <li>
         <span><?php the_category(' &bull; '); ?></span>
+    </li>
+    <li>
+        <a title="<?php the_permalink(); ?>" href="<?php the_permalink(); ?>">Permalink</a>
     </li>
     <li class="comments">
         <a title="Jump to the comments &raquo;" href="<?php comments_link(); ?>"><?php comments_number( '', 'One comment', '% comments' ); ?></a>
