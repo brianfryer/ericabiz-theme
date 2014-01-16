29b66a14329cc3d009cd74f40ec2c83f6480bb13
Brian Fryer <brianfryer@gmail.com>
2014-01-16 12:52:03 -0600

    Removing superfluous semi-colon from the footer.

--- footer.php
+++ footer.php
@@ -39,7 +39,7 @@
 
 <?php wp_footer(); ?>
 
-    <script type="text/javascript" src="http://analytics.aweber.com/js/awt_analytics.js?id=4IsS"></script>;
+    <script type="text/javascript" src="http://analytics.aweber.com/js/awt_analytics.js?id=4IsS"></script>
 
 </body>
 </html>
